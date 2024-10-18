2026d3bdda3cf5e3bdb715e8b0020ab5545d2817
emsheesh69 <ejmntclr22@gmail.com>
2024-10-18 19:52:49 +0800

    updated with fixed design, and regis

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.auth.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     implementation ("com.google.android.material:material:1.9.0")
